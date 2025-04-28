6ac35172439c2adbce26c70686fc5f06c2f738b6
jerry <jerrytekton@gmail.com>
2025-04-28 13:21:58 +1200

    test argocd

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -79,7 +79,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion(version);
+    coloring.setVersion("v3");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
