f70614911007e851b1b5d30b91c379256a15e8b7
jerry <jerrytekton@gmail.com>
2025-04-28 13:20:29 +1200

    change version for test deploy to argocd

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -79,7 +79,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion(version);
+    coloring.setVersion("v1");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
