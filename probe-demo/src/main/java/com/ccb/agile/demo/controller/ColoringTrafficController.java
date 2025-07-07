d0286cbb08c2310e875b7f35a2a48958e48c4072
jerryTJ <jerrytekton@gmail.com>
2025-07-07 15:08:06 +1200

    Update ColoringTrafficController.java

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -78,7 +78,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion("v3-2");
+    coloring.setVersion("v3-3");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
