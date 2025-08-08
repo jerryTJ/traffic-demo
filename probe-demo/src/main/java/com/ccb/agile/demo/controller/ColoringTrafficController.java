1850a48db61dcd645291e38def42526ac7ef0595
Jerry Liu <jerryliu@jerry.liu>
2025-08-08 17:33:28 +1200

    test helm workflow

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -78,7 +78,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion("v3-4");
+    coloring.setVersion("v3-helm");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
