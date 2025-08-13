892e7bd6e5bf5d4620608afc093b83906a22c05c
Jerry Liu <jerryliu@jerry.liu>
2025-08-13 15:02:30 +1200

    fetch test

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -78,7 +78,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion("v3-scrum-2631");
+    coloring.setVersion("v3-scrum-2631-test-fetch");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
