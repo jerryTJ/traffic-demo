040f6ed2d2e54f0af99364273762edb992992dec
Jerry Liu <jerryliu@jerry.liu>
2025-08-14 15:42:11 +1200

    test helm tag

--- probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
+++ probe-demo/src/main/java/com/ccb/agile/demo/controller/ColoringTrafficController.java
@@ -78,7 +78,7 @@ public class ColoringTrafficController {
     coloring.setChainId(chainId);
     coloring.setColor(color);
     coloring.setHostName(hostName);
-    coloring.setVersion("v3-test-release");
+    coloring.setVersion("v1");
     coloring.setNext(null);
     return ResponseEntity.ok(coloring);
   }
